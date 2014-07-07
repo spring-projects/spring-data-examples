dce2994f1fe4bc9d0dcb25b584910dd764275eb9
Oliver Gierke <ogierke@gopivotal.com>
2014-07-07 19:37:45 +0200

    Prepare security integration examples.

---
+++ jpa/security/src/test/java/example/springdata/jpa/security/SecurityIntegrationTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jpa.security;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Integration test to show the usage of Java 8 date time APIs with Spring Data JPA auditing.
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SecurityConfiguration.class)
+@Transactional
+public class SecurityIntegrationTests {
+
+}
