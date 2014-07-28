bb8711598e6fda6379e0363b7e8919dfb843b16a
Oliver Gierke <ogierke@gopivotal.com>
2014-07-28 20:01:44 +0200

    Added example for multi-store configuration.

---
+++ multi-store/src/test/java/example/springdata/multistore/ApplicationConfigurationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.multistore;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import example.springdata.multistore.customer.Customer;
+import example.springdata.multistore.shop.Order;
+
+/**
+ * Integration test to check repository interfaces are assigned to the correct store modules.
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ApplicationConfiguration.class)
+public class ApplicationConfigurationTest {
+
+	@Autowired ApplicationContext context;
+
+	@Test
+	public void repositoriesAreAssignedToAppropriateStores() {
+
+		Repositories repositories = new Repositories(context);
+
+		assertThat(repositories.getEntityInformationFor(Customer.class), is(instanceOf(JpaEntityInformation.class)));
+		assertThat(repositories.getEntityInformationFor(Order.class), is(instanceOf(MongoEntityInformation.class)));
+	}
+}
