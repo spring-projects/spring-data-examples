3d44d2837b6b6ad4a36515b91101e08eb1a6153e
Christoph Strobl <cstrobl@pivotal.io>
2014-09-08 12:20:26 -0500

    Issue #8 - Add fallback for initializing repository when offline.
    Remove online resources and just rely on local data. Though we had to keep the BlogPostInitializer for setting up the tests only using the template without repository support.

--- mongodb/text-search/src/test/java/example/springdata/mongodb/util/BlogPostInitializer.java
+++ mongodb/text-search/src/test/java/example/springdata/mongodb/util/BlogPostInitializer.java
@@ -15,25 +15,12 @@
  */
 package example.springdata.mongodb.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.repository.init.Jackson2ResourceReader;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
-
-import com.rometools.rome.feed.atom.Category;
-import com.rometools.rome.feed.atom.Content;
-import com.rometools.rome.feed.atom.Entry;
-import com.rometools.rome.feed.atom.Feed;
 
 import example.springdata.mongodb.textsearch.BlogPost;
 
@@ -48,10 +35,6 @@ public enum BlogPostInitializer {
 
 	INSTANCE;
 
-	private final RestTemplate restTemplate = new RestTemplate();
-	private final Converter<Entry, BlogPost> converter = EntryConverter.INSTANCE;
-	private final String url = "https://spring.io/blog.atom";
-
 	/**
 	 * Initializes the given {@link MongoOperations} with {@link BlogPost}s from the Spring Blog.
 	 * 
@@ -61,29 +44,7 @@ public enum BlogPostInitializer {
 	public void initialize(MongoOperations operations) throws Exception {
 
 		Assert.notNull(operations, "MongoOperations must not be null!");
-
-		try {
-			loadFromOnlineSource(operations);
-		} catch (HttpServerErrorException | ResourceAccessException error) {
-			loadFromClasspathSource(operations);
-		}
-	}
-
-	private void loadFromOnlineSource(MongoOperations operations) {
-
-		ResponseEntity<Feed> feed = restTemplate.getForEntity(url, Feed.class);
-		int count = 0;
-
-		if (feed.hasBody()) {
-			for (Object entry : feed.getBody().getEntries()) {
-				if (entry instanceof Entry) {
-					operations.save(converter.convert((Entry) entry));
-					count++;
-				}
-			}
-		}
-
-		log.info("Imported {} blog posts from spring.io!", count);
+		loadFromClasspathSource(operations);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -91,8 +52,7 @@ public enum BlogPostInitializer {
 
 		Jackson2ResourceReader reader = new Jackson2ResourceReader();
 
-		Object source = reader.readFrom(new ClassPathResource(
-				"/example/springdata/mongodb/textsearch/spring-blog.atom.json"), this.getClass().getClassLoader());
+		Object source = reader.readFrom(new ClassPathResource("spring-blog.atom.json"), this.getClass().getClassLoader());
 
 		if (source instanceof Iterable) {
 			((Iterable) source).forEach(element -> operations.save(element));
@@ -100,48 +60,6 @@ public enum BlogPostInitializer {
 			operations.save(source);
 		}
 
-		log.info("Imported 20 blog posts from classpath!");
-	}
-
-	/**
-	 * {@link Converter} implementation capable of converting atom feed {@link Entry} into {@link BlogPost}.
-	 * 
-	 * @author Christoph Strobl
-	 * @author Oliver Gierke
-	 */
-	private enum EntryConverter implements Converter<Entry, BlogPost> {
-
-		INSTANCE;
-
-		/*
-		 * (non-Javadoc)
-		 * @see org.springframework.core.convert.converter.Converter#convert(java.lang.Object)
-		 */
-		@Override
-		public BlogPost convert(Entry source) {
-
-			BlogPost post = new BlogPost();
-
-			post.setId(source.getId());
-			post.setTitle(source.getTitle());
-
-			for (Object content : source.getContents()) {
-				if (content instanceof Content) {
-					post.setContent(((Content) content).getValue());
-				}
-			}
-
-			List<String> categories = new ArrayList<String>();
-
-			for (Object category : source.getCategories()) {
-				if (category instanceof Category) {
-					categories.add(((Category) category).getLabel());
-				}
-			}
-
-			post.setCategories(categories);
-
-			return post;
-		}
+		log.info("Imported blog posts from classpath!");
 	}
 }
