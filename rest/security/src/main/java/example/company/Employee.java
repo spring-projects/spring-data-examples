5051aa1dfa255cb8e9b63df5e8ef1820bfc7ae94
Greg Turnquist <gturnquist@gopivotal.com>
2014-10-10 11:40:08 -0500

    Resolves #21

---
+++ rest/security/src/main/java/example/company/Employee.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.company;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+@Entity
+public class Employee {
+
+	@Id @GeneratedValue
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	private String title;
+
+	/**
+	 * This constructor is needed to support JPA, but made
+	 * private to force clients to use the other one.
+	 */
+	private Employee() {
+
+	}
+
+	public Employee(String firstName, String lastName, String title) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.title = title;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
