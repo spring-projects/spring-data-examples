0f7afacafceb835b77509721a9edbbd6fc416ec7
Oliver Gierke <ogierke@pivotal.io>
2016-07-29 18:04:51 +0200

    #204 - Upgraded to Spring Boot 1.4 GA.

--- jpa/java8/src/main/java/example/springdata/jpa/java8/AuditingConfiguration.java
+++ jpa/java8/src/main/java/example/springdata/jpa/java8/AuditingConfiguration.java
@@ -16,13 +16,10 @@
 package example.springdata.jpa.java8;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@EntityScan(basePackageClasses = { AuditingConfiguration.class, Jsr310JpaConverters.class })
 @EnableJpaAuditing
 class AuditingConfiguration {}
--- web/projection/src/test/java/example/users/UserControllerIntegrationTests.java
+++ web/projection/src/test/java/example/users/UserControllerIntegrationTests.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -36,7 +37,7 @@ import org.springframework.test.web.servlet.ResultActions;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc(alwaysPrint = false)
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
 public class UserControllerIntegrationTests {
 
 	@Autowired MockMvc mvc;
