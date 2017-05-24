829937583291f7b4d677bb7ec2d7db5d0d72cf03
Mark Paluch <mpaluch@pivotal.io>
2017-05-24 15:50:18 +0200

    Adopt Cassandra examples to relocated packages.

--- cassandra/java8/src/main/java/example/springdata/cassandra/java8/Order.java
+++ cassandra/java8/src/main/java/example/springdata/cassandra/java8/Order.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.cassandra.mapping.Table;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 /**
  * @author Mark Paluch
--- cassandra/java8/src/main/java/example/springdata/cassandra/java8/Person.java
+++ cassandra/java8/src/main/java/example/springdata/cassandra/java8/Person.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package example.springdata.cassandra.java8;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.cassandra.mapping.Table;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
