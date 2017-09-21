50945ac434cd3b9b04e60ea608f5ead4209f9f07
Oliver Gierke <ogierke@pivotal.io>
2017-09-21 16:45:55 +0200

    #297 - Upgraded to Thymeleaf Spring Data dialect compatible with Thymeleaf 3.

--- web/querydsl/src/main/java/example/users/Application.java
+++ web/querydsl/src/main/java/example/users/Application.java
@@ -21,7 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 /**
@@ -29,7 +29,7 @@ import org.thymeleaf.dialect.springdata.SpringDataDialect;
  * @author Oliver Gierke
  */
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
