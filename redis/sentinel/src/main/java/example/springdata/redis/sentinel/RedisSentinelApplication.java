6f3dd29431b69e1ac498ac21ba056b00e91d524f
Oliver Gierke <ogierke@pivotal.io>
2017-10-06 17:30:36 +0200

    #297 - Re-enable Redis reactive example.
    Removed fixed versions of dependencies. Removed explicit declaration of Lettuce version as it is now managed by Boot. Switched to Lettuce as primary Redis driver in application setup. We keep using Jedis in the embedded server support.

--- redis/sentinel/src/main/java/example/springdata/redis/sentinel/RedisSentinelApplication.java
+++ redis/sentinel/src/main/java/example/springdata/redis/sentinel/RedisSentinelApplication.java
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.StopWatch;
 
@@ -74,7 +74,7 @@ public class RedisSentinelApplication {
 	}
 
 	public @Bean RedisConnectionFactory connectionFactory() {
-		return new JedisConnectionFactory(sentinelConfig());
+		return new LettuceConnectionFactory(sentinelConfig());
 	}
 
 	public @Bean RedisSentinelConfiguration sentinelConfig() {
