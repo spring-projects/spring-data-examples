19cf46330500f7fa2b1f22a1efc21845214cfa52
Mark Paluch <mpaluch@pivotal.io>
2017-11-30 15:03:49 -0800

    #327 - Add Jackson example for Reactive Redis use.

--- redis/reactive/src/test/java/example/springdata/redis/RedisTestConfiguration.java
+++ redis/reactive/src/test/java/example/springdata/redis/RedisTestConfiguration.java
@@ -24,7 +24,11 @@ import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializationContext.RedisSerializationContextBuilder;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author Mark Paluch
@@ -42,6 +46,39 @@ public class RedisTestConfiguration {
 	@Bean
 	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
 		return new ReactiveRedisTemplate<>(connectionFactory, RedisSerializationContext.string());
+	}
+
+	/**
+	 * Configures a {@link ReactiveRedisTemplate} with {@link String} keys and a typed
+	 * {@link Jackson2JsonRedisSerializer}.
+	 */
+	@Bean
+	public ReactiveRedisTemplate<String, Person> reactiveJsonPersonRedisTemplate(
+			ReactiveRedisConnectionFactory connectionFactory) {
+
+		Jackson2JsonRedisSerializer<Person> serializer = new Jackson2JsonRedisSerializer<>(Person.class);
+		RedisSerializationContextBuilder<String, Person> builder = RedisSerializationContext
+				.newSerializationContext(new StringRedisSerializer());
+
+		RedisSerializationContext<String, Person> serializationContext = builder.value(serializer).build();
+
+		return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
+	}
+
+	/**
+	 * Configures a {@link ReactiveRedisTemplate} with {@link String} keys and {@link GenericJackson2JsonRedisSerializer}.
+	 */
+	@Bean
+	public ReactiveRedisTemplate<String, Object> reactiveJsonObjectRedisTemplate(
+			ReactiveRedisConnectionFactory connectionFactory) {
+
+		RedisSerializationContextBuilder<String, Object> builder = RedisSerializationContext
+				.newSerializationContext(new StringRedisSerializer());
+
+		RedisSerializationContext<String, Object> serializationContext = builder
+				.value(new GenericJackson2JsonRedisSerializer("_type")).build();
+
+		return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
 	}
 
 	/**
