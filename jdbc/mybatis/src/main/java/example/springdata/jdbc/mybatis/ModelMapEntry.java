047610df5f9407c3fefd5b7cf9b188a474fb8a97
Jens Schauder <jschauder@pivotal.io>
2017-12-01 10:56:40 +0100

    #314 - Added MyBatis example.
    MyBatisTests demonstrates how some queries executed by Spring Data JDBC can be replaced with MyBatis mappings.
    The domain model is based on the basic JDBC.
    
    The map of models is maintained by two statements configured in MyBatis mappings.
    
    example.springdata.jdbc.mybatis.LegoSetMapper.findAllByProperty-models showcases how a map can be loaded by configuring the select to return instances of Map.Entry
    
    example.springdata.jdbc.mybatis.Model.insert showcases how one can access the MyBatixContext and thereby the instance to save and the key of the parent entity.
    
    Original pull request: #345.

---
+++ jdbc/mybatis/src/main/java/example/springdata/jdbc/mybatis/ModelMapEntry.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jdbc.mybatis;
+
+import java.sql.Clob;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * @author Jens Schauder
+ */
+public class ModelMapEntry implements Map.Entry<String, Model> {
+
+	private final String key;
+	private final Model value;
+
+	ModelMapEntry(String name, Clob description) {
+
+		key = name;
+		value = new Model();
+		value.name = name;
+		try {
+			value.description = description.getSubString(1, (int) description.length());
+		} catch (SQLException se) {
+			throw new RuntimeException(se);
+		}
+	}
+
+	@Override
+	public String getKey() {
+		return key;
+	}
+
+	@Override
+	public Model getValue() {
+		return value;
+	}
+
+	@Override
+	public Model setValue(Model value) {
+		throw new UnsupportedOperationException("can't set the value of a ModelMapEntry");
+	}
+}
