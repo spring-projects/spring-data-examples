04be1b9784fda38958576a9afd502356b7c3cf95
Mark Paluch <mpaluch@pivotal.io>
2018-09-13 12:24:49 +0200

    #408 - Remove Dev-Tools exclusions for multiple datasources.
    Cleanup our examples as the issues that required exclusion are solved now.

--- jpa/multiple-datasources/src/main/java/example/springdata/jpa/multipleds/Application.java
+++ jpa/multiple-datasources/src/main/java/example/springdata/jpa/multipleds/Application.java
@@ -22,30 +22,17 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.devtools.autoconfigure.DevToolsDataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * Core Spring Boot application configuration. Note, that we explicitly deactivate some auto-configurations explicitly.
- * They mostly will even disable automatically if special bean names are used (e.g. {@code entityManagerFactory}) but I
- * wanted to keep the two configurations symmetric. The configuration classes being located in separate packages serves
- * the purpose of scoping the Spring Data repository scanning to those packages so that the infrastructure setup is
- * attached to the corresponding repository instances.
- * <p>
- * {@link DevToolsDataSourceAutoConfiguration} is explicitly excluded until
- * {@link https://github.com/spring-projects/spring-boot/issues/5540} is fixed.
- * {@link https://github.com/spring-projects/spring-boot/issues/5541} has been filed to improve the need for manual
- * exclusions in general.
+ * Core Spring Boot application configuration.
  *
  * @author Oliver Gierke
+ * @author Mark Paluch
  * @see example.springdata.jpa.multipleds.customer.CustomerConfig
  * @see example.springdata.jpa.multipleds.order.OrderConfig
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class })
+@SpringBootApplication
 @EnableTransactionManagement
 public class Application {
 
