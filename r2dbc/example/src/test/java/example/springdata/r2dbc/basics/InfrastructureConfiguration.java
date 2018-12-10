b38ade2298fd61a5db0ac1b7f3dfa3531fc6f4fc
Mark Paluch <mpaluch@pivotal.io>
2018-12-10 08:39:37 +0100

    #438 - Adapt to refactored R2DBC configuration.
    AbstractR2dbcConfiguration was moved from org.springframework.data.r2dbc.repository.config.AbstractR2dbcConfiguration to org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration.

--- r2dbc/example/src/test/java/example/springdata/r2dbc/basics/InfrastructureConfiguration.java
+++ r2dbc/example/src/test/java/example/springdata/r2dbc/basics/InfrastructureConfiguration.java
@@ -22,7 +22,7 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.repository.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.testcontainers.containers.PostgreSQLContainer;
 
