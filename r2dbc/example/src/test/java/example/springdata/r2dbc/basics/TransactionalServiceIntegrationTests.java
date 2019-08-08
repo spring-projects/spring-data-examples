616f7dad620b42147952af4e1d343ee42a8f9d13
Oliver Drotbohm <odrotbohm@pivotal.io>
2019-08-08 09:07:47 +0200

    #514 - Switch to latest Spring Data R2DBC snapshots.
    This is to accommodate API refactorings in Spring Framework's transaction API.

--- r2dbc/example/src/test/java/example/springdata/r2dbc/basics/TransactionalServiceIntegrationTests.java
+++ r2dbc/example/src/test/java/example/springdata/r2dbc/basics/TransactionalServiceIntegrationTests.java
@@ -52,8 +52,7 @@ public class TransactionalServiceIntegrationTests {
 				"DROP TABLE IF EXISTS customer;",
 				"CREATE TABLE customer ( id SERIAL PRIMARY KEY, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL);");
 
-		statements.forEach(it -> database.execute() //
-				.sql(it) //
+		statements.forEach(it -> database.execute(it) //
 				.fetch() //
 				.rowsUpdated() //
 				.as(StepVerifier::create) //
