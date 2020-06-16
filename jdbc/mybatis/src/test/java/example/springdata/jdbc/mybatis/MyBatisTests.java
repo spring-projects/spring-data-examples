3d88c50655d2e20c0445ea253f5ccc77f019f8bf
Mark Paluch <mpaluch@pivotal.io>
2020-06-16 13:47:43 +0200

    #565 - Re-enable JDBC MyBatis examples after.
    Neumann SR1 ships with a fix for the MyBatis integration.

--- jdbc/mybatis/src/test/java/example/springdata/jdbc/mybatis/MyBatisTests.java
+++ jdbc/mybatis/src/test/java/example/springdata/jdbc/mybatis/MyBatisTests.java
@@ -1,12 +1,11 @@
-package example.springdata.jdbc.mybatis;
 /*
- * Copyright 2017-2018 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +13,10 @@ package example.springdata.jdbc.mybatis;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package example.springdata.jdbc.mybatis;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -40,7 +39,6 @@ public class MyBatisTests {
 	@Autowired LegoSetRepository repository;
 
 	@Test
-	@Ignore // this is currently broken until the fix for DATAJDBC-542 is released.
 	public void exerciseSomewhatComplexEntity() {
 
 		LegoSet smallCar = createLegoSet();
