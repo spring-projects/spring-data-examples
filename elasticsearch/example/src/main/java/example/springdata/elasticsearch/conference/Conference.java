b6a2754e6a12f054b004e931bb54d40d6797e678
Mark Paluch <mpaluch@pivotal.io>
2020-08-14 11:58:31 +0200

    #582 - Adopt to changes in Spring Data Elasticsearch 3.1.

--- elasticsearch/example/src/main/java/example/springdata/elasticsearch/conference/Conference.java
+++ elasticsearch/example/src/main/java/example/springdata/elasticsearch/conference/Conference.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014-2018 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,12 +30,11 @@ import lombok.Data;
 /**
  * @author Artur Konczak
  * @author Oliver Gierke
- * @auhtor Christoph Strobl
+ * @author Christoph Strobl
  */
 @Data
 @Builder
-@Document(indexName = "conference-index", type = "geo-class-point-type", shards = 1, replicas = 0,
-		refreshInterval = "-1")
+@Document(indexName = "conference-index")
 public class Conference {
 
 	private @Id String id;
