cb38fd8476fe27e3c5cba5953ce0277e738c421b
Mark Paluch <mpaluch@pivotal.io>
2020-11-04 14:26:18 +0100

    #585 - Polishing.
    Remove superfluous files. Tweak Javadoc.
    
    Original pull request: #586.

--- mongodb/repository-metrics/src/test/java/example/springdata/mongodb/DemoApplicationTests.java
+++ mongodb/repository-metrics/src/test/java/example/springdata/mongodb/DemoApplicationTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,9 +21,16 @@ import reactor.test.StepVerifier;
 import java.time.Duration;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Integration tests showing {@link org.springframework.data.repository.core.support.RepositoryMethodInvocationListener}
+ * using query method in action.
+ *
+ * @author Christoph Strobl
+ */
 @SpringBootTest
 class DemoApplicationTests {
 
