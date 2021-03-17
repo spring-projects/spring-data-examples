3da54402c25ed9e03feb6072c798440ac93978a9
Gerrit Meier <meistermeier@gmail.com>
2021-03-17 09:29:24 +0100

    Update Neo4j example.
    Spring Data Neo4j 6.1 will enforce the usage of
    an identifier in relationship properties.
    Otherwise unmapped properties of relationships
    could get overwritten.

--- neo4j/example/src/main/java/example/springdata/neo4j/Roles.java
+++ neo4j/example/src/main/java/example/springdata/neo4j/Roles.java
@@ -20,6 +20,8 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
@@ -31,6 +33,10 @@ import org.springframework.data.neo4j.core.schema.TargetNode;
  */
 @RelationshipProperties
 public class Roles {
+
+	@Id
+	@GeneratedValue
+	private Long id;
 
 	private final List<String> roles;
 
