43c2d3c7840826a05e66488e736928412dc4f0ee
Mark Paluch <mpaluch@vmware.com>
2021-04-13 11:41:44 +0200

    Polishing.
    Refactor sample data into saveAll(…) for easier alignment of data changes across samples.
    
    See #610
    Original pull request: #612.

--- elasticsearch/example/src/main/java/example/springdata/elasticsearch/conference/ApplicationConfiguration.java
+++ elasticsearch/example/src/main/java/example/springdata/elasticsearch/conference/ApplicationConfiguration.java
@@ -16,6 +16,7 @@
 package example.springdata.elasticsearch.conference;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -48,16 +49,20 @@ class ApplicationConfiguration {
 		operations.indexOps(Conference.class).refresh();
 
 		// Save data sample
-		repository.save(Conference.builder().date("2014-11-06").name("Spring eXchange 2014 - London")
-				.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(51.500152D, -0.126236D)).build());
-		repository.save(Conference.builder().date("2014-12-07").name("Scala eXchange 2014 - London")
-				.keywords(Arrays.asList("scala", "play", "java")).location(new GeoPoint(51.500152D, -0.126236D)).build());
-		repository.save(Conference.builder().date("2014-11-20").name("Elasticsearch 2014 - Berlin")
-				.keywords(Arrays.asList("java", "elasticsearch", "kibana")).location(new GeoPoint(52.5234051D, 13.4113999))
-				.build());
-		repository.save(Conference.builder().date("2014-11-12").name("AWS London 2014")
-				.keywords(Arrays.asList("cloud", "aws")).location(new GeoPoint(51.500152D, -0.126236D)).build());
-		repository.save(Conference.builder().date("2014-10-04").name("JDD14 - Cracow")
-				.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(50.0646501D, 19.9449799)).build());
+
+		List<Conference> documents = Arrays.asList(
+				Conference.builder().date("2014-11-06").name("Spring eXchange 2014 - London")
+						.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-12-07").name("Scala eXchange 2014 - London")
+						.keywords(Arrays.asList("scala", "play", "java")).location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-11-20").name("Elasticsearch 2014 - Berlin")
+						.keywords(Arrays.asList("java", "elasticsearch", "kibana")).location(new GeoPoint(52.5234051D, 13.4113999))
+						.build(), //
+				Conference.builder().date("2014-11-12").name("AWS London 2014").keywords(Arrays.asList("cloud", "aws"))
+						.location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-10-04").name("JDD14 - Cracow").keywords(Arrays.asList("java", "spring"))
+						.location(new GeoPoint(50.0646501D, 19.9449799)).build());
+
+		repository.saveAll(documents);
 	}
 }
--- elasticsearch/reactive/src/main/java/example/springdata/elasticsearch/conference/ApplicationConfiguration.java
+++ elasticsearch/reactive/src/main/java/example/springdata/elasticsearch/conference/ApplicationConfiguration.java
@@ -19,14 +19,15 @@ import reactor.test.StepVerifier;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.elasticsearch.ElasticsearchStatusException;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.indices.CreateIndexRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -64,26 +65,21 @@ class ApplicationConfiguration {
 			// just ignore it
 		}
 
-		// Remove all documents
-		repository.deleteAll().subscribe();
+		List<Conference> documents = Arrays.asList(
+				Conference.builder().date("2014-11-06").name("Spring eXchange 2014 - London")
+						.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-12-07").name("Scala eXchange 2014 - London")
+						.keywords(Arrays.asList("scala", "play", "java")).location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-11-20").name("Elasticsearch 2014 - Berlin")
+						.keywords(Arrays.asList("java", "elasticsearch", "kibana")).location(new GeoPoint(52.5234051D, 13.4113999))
+						.build(), //
+				Conference.builder().date("2014-11-12").name("AWS London 2014").keywords(Arrays.asList("cloud", "aws"))
+						.location(new GeoPoint(51.500152D, -0.126236D)).build(), //
+				Conference.builder().date("2014-10-04").name("JDD14 - Cracow").keywords(Arrays.asList("java", "spring"))
+						.location(new GeoPoint(50.0646501D, 19.9449799)).build());
 
-		// Save data sample
-		repository
-				.save(Conference.builder().date("2014-11-06").name("Spring eXchange 2014 - London")
-						.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(51.500152D, -0.126236D)).build())
-				.then().as(StepVerifier::create).verifyComplete();
-		repository
-				.save(Conference.builder().date("2014-12-07").name("Scala eXchange 2014 - London")
-						.keywords(Arrays.asList("scala", "play", "java")).location(new GeoPoint(51.500152D, -0.126236D)).build())
-				.then().as(StepVerifier::create).verifyComplete();
-		repository.save(Conference.builder().date("2014-11-20").name("Elasticsearch 2014 - Berlin")
-				.keywords(Arrays.asList("java", "elasticsearch", "kibana")).location(new GeoPoint(52.5234051D, 13.4113999))
-				.build()).then().as(StepVerifier::create).verifyComplete();
-		repository.save(Conference.builder().date("2014-11-12").name("AWS London 2014")
-				.keywords(Arrays.asList("cloud", "aws")).location(new GeoPoint(51.500152D, -0.126236D)).build()).then()
-				.as(StepVerifier::create).verifyComplete();
-		repository.save(Conference.builder().date("2014-10-04").name("JDD14 - Cracow")
-				.keywords(Arrays.asList("java", "spring")).location(new GeoPoint(50.0646501D, 19.9449799)).build()).then()
+		// Remove all documents
+		repository.deleteAll().then(repository.saveAll(documents).then())
 				.as(StepVerifier::create).verifyComplete();
 	}
 }
