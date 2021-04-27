eefc3b770be1a0a13db0ec66dde836c162ad65eb
divya_jnu08 <divya@virtueanalytics.com>
2021-04-27 20:30:25 +0530

    Migrating map examples to junit5

--- map/src/test/java/example/springdata/map/PersonRepositoryIntegrationTest.java
+++ map/src/test/java/example/springdata/map/PersonRepositoryIntegrationTest.java
@@ -19,20 +19,19 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.map.repository.config.EnableMapRepositories;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 /**
  * Integration tests for {@link PersonRepository}.
  *
  * @author Oliver Gierke
  */
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class PersonRepositoryIntegrationTest {
 
