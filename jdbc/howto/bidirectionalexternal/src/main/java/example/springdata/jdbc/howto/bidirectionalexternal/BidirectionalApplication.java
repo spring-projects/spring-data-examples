387ca8c6c3585af1bc12d6ec231776e0f7ca6810
Jens Schauder <jschauder@vmware.com>
2021-09-17 10:43:59 +0200

    Adds examples how to model bidirectional relationships.
    Original pull request #629

---
+++ jdbc/howto/bidirectionalexternal/src/main/java/example/springdata/jdbc/howto/bidirectionalexternal/BidirectionalApplication.java
@@ -0,0 +1,13 @@
+package example.springdata.jdbc.howto.bidirectionalexternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidirectionalApplication.class, args);
+	}
+
+}
---
+++ jdbc/howto/bidirectionalinternal/src/main/java/example/springdata/jdbc/howto/bidirectionalinternal/BiDiInternalApplication.java
@@ -0,0 +1,13 @@
+package example.springdata.jdbc.howto.bidirectionalinternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BiDiInternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiDiInternalApplication.class, args);
+	}
+
+}
