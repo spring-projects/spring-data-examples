d172c56dde306245c14ab1b78f9c1688f43fe5de
Christoph Strobl <cstrobl@vmware.com>
2022-10-12 09:58:24 +0200

    Add native data-jdbc sample.
    See #654

---
+++ jdbc/graalvm-native/src/test/java/example/springdata/jdbc/graalvmnative/GraalvmNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.springdata.jdbc.graalvmnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
