15783e4f7c14f1dfbe092d88fe8f5fd4be4f4afa
Jens Schauder <jschauder@vmware.com>
2023-11-06 11:39:52 +0100

    Add examples to use Single Query Loading.
    Query by Id works via the repository.
    
    Query derivation doesn't since it side steps the DataAccessStrategy.
    
    Closes #675

---
+++ jdbc/singlequeryloading/src/main/java/example/springdata/jdbc/singlequeryloading/Config.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jdbc.singlequeryloading;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
+import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.relational.RelationalManagedTypes;
+import org.springframework.data.relational.core.mapping.NamingStrategy;
+
+import java.util.Optional;
+
+/**
+ * Spring application context configuration that enables Single Query Loading.
+ *
+ * @author Jens Schauder
+ */
+@SpringBootConfiguration
+@EnableJdbcRepositories
+public class Config extends AbstractJdbcConfiguration {
+
+
+	@Override
+	public JdbcMappingContext jdbcMappingContext(Optional<NamingStrategy> namingStrategy, JdbcCustomConversions customConversions, RelationalManagedTypes jdbcManagedTypes) {
+
+		JdbcMappingContext jdbcMappingContext = super.jdbcMappingContext(namingStrategy, customConversions, jdbcManagedTypes);
+		jdbcMappingContext.setSingleQueryLoadingEnabled(true);
+		return jdbcMappingContext;
+	}
+}
---
+++ jdbc/singlequeryloading/src/test/java/example/springdata/jdbc/singlequeryloading/SingleQueryLoadingApplicationTests.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jdbc.singlequeryloading;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.CriteriaDefinition;
+import org.springframework.data.relational.core.query.Query;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Run tests demonstrating the use of Single Query Loading. You'll have to observe the executed queries.
+ *
+ * @author Jens Schauder
+ */
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SingleQueryLoadingApplicationTests {
+
+	@Autowired
+	PetOwnerRepository petOwners;
+	@Autowired
+	JdbcAggregateTemplate template;
+	private PetOwner emil;
+	private PetOwner marry;
+
+	@BeforeEach
+	void setup() {
+		petOwners.deleteAll();
+		emil = petOwners.save(new PetOwner("Emil",
+				List.of(
+						new Cat("Edgar"),
+						new Cat("Einstein"),
+						new Cat("Elliot"),
+						new Cat("Elton"),
+						new Cat("Evan")
+				),
+				List.of(
+						new Dog("Eric"),
+						new Dog("Eddie"),
+						new Dog("Eke"),
+						new Dog("Echo")
+				),
+				List.of(
+						new Fish("Floaty")
+				)
+
+		));
+
+		marry = petOwners.save(new PetOwner("Marry",
+				List.of(
+						new Cat("Mars"),
+						new Cat("Maverick"),
+						new Cat("Max")
+				),
+				List.of(
+						new Dog("Molly"),
+						new Dog("Murphy"),
+						new Dog("Madison"),
+						new Dog("Macie")
+				),
+				List.of(
+						new Fish("Mahi Mahi"),
+						new Fish("Mr. Limpet")
+				)
+		));
+	}
+
+	@Test
+	void loadById() {
+
+		PetOwner emilReloaded = petOwners.findById(emil.Id).orElseThrow();
+
+		assertThat(emilReloaded).isEqualTo(emil);
+
+	}
+
+	@Test
+	void loadByName() {
+
+		CriteriaDefinition criteria = Criteria.where("name").is("Marry");
+		Query query = Query.query(criteria);
+		List<PetOwner> marries = (List<PetOwner>) template.findAll(query, PetOwner.class);
+		assertThat(marries).containsExactly(marry);
+	}
+
+}
