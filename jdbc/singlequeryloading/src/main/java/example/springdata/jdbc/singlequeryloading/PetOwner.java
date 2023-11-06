7e710ccd84116b0f964852a2000b75d28098182f
Jens Schauder <jschauder@vmware.com>
2023-11-06 11:39:52 +0100

    Add examples to use Single Query Loading.
    Query by Id works via the repository.
    
    Query derivation doesn't since it side steps the DataAccessStrategy.

---
+++ jdbc/singlequeryloading/src/main/java/example/springdata/jdbc/singlequeryloading/PetOwner.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package example.springdata.jdbc.singlequeryloading;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * An aggregate with mutliple collections.
+ *
+ * @author Jens Schauder
+ */
+class PetOwner {
+
+	@Id
+	Long Id;
+
+	String name;
+
+	List<Dog> dogs = new ArrayList<>();
+
+	List<Cat> cats = new ArrayList<>();
+
+	List<Fish> fish = new ArrayList<>();
+
+	public PetOwner(String name, List<Cat> cats, List<Dog> dogs, List<Fish> fish) {
+
+		this.name = name;
+		this.cats = cats;
+		this.dogs = dogs;
+		this.fish = fish;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		PetOwner petOwner = (PetOwner) o;
+		return Objects.equals(Id, petOwner.Id) && Objects.equals(name, petOwner.name) && Objects.equals(dogs, petOwner.dogs) && Objects.equals(cats, petOwner.cats) && Objects.equals(fish, petOwner.fish);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id, name, dogs, cats, fish);
+	}
+}
