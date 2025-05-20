b0edc60a66fa6e59eb3fb9bf7e7bf0669cbb40c5
Christoph Strobl <christoph.strobl@broadcom.com>
2025-05-20 07:41:37 +0200

    Add MongoDB vector search example.

---
+++ mongodb/util/src/main/java/example/springdata/mongodb/util/AtlasContainerConnectionDetailsFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.mongodb.util;
+
+import com.mongodb.ConnectionString;
+import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.testcontainers.service.connection.ContainerConnectionDetailsFactory;
+import org.springframework.boot.testcontainers.service.connection.ContainerConnectionSource;
+
+public class AtlasContainerConnectionDetailsFactory extends ContainerConnectionDetailsFactory<AtlasContainer, MongoConnectionDetails> {
+
+    AtlasContainerConnectionDetailsFactory() {
+        super(ANY_CONNECTION_NAME, new String[]{"com.mongodb.ConnectionString"});
+    }
+
+    protected MongoConnectionDetails getContainerConnectionDetails(ContainerConnectionSource<AtlasContainer> source) {
+        return new MongoContainerConnectionDetails(source);
+    }
+
+    private static final class MongoContainerConnectionDetails extends ContainerConnectionDetailsFactory.ContainerConnectionDetails<AtlasContainer> implements MongoConnectionDetails {
+
+        private MongoContainerConnectionDetails(ContainerConnectionSource<AtlasContainer> source) {
+            super(source);
+        }
+
+        public ConnectionString getConnectionString() {
+            return new ConnectionString(this.getContainer().getConnectionString());
+        }
+
+        public SslBundle getSslBundle() {
+            return super.getSslBundle();
+        }
+    }
+}
+
---
+++ mongodb/vector-search/src/main/java/example/springdata/vector/Comment.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.vector;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Vector;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Sample entity containing a {@link Vector vector} {@link #embedding}.
+ */
+@Document
+public class Comment {
+
+    private ObjectId id;
+
+    private String country;
+    private String description;
+
+    private Vector embedding;
+
+    public Comment() {
+    }
+
+    public Comment(String country, String description, Vector embedding) {
+        this.country = country;
+        this.description = description;
+        this.embedding = embedding;
+    }
+
+    public static Comment of(Comment source) {
+        return new Comment(source.getCountry(), source.getDescription(), source.getEmbedding());
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Vector getEmbedding() {
+        return embedding;
+    }
+
+
+    @Override
+    public String toString() {
+        return "%s (%s)".formatted(getDescription(), getCountry());
+    }
+}
